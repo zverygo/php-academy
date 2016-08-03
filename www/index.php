110ea74d99e9264c67ae7cbb04b9c59ca02483fc
zverygo <simogin.roman@gmail.com>
2016-08-03 20:53:34 +0300

    сделал кнопку

--- www/index.php
+++ www/index.php
@@ -1,7 +1,20 @@
 <?php
-
-echo "less2.<br>";
-echo date('d m Y');
-
-
+echo "less2.<hr>";
 ?>
+
+
+<style>
+    .butt {
+        color: darkred;
+        background-color: aqua;
+        text-align: center;
+        width: 100px;
+        height: 35px;
+        border: none;
+    }
+
+</style>
+
+<form action="index.php">
+    <button class="butt" type="submit"> SAVE </button>
+</form>
