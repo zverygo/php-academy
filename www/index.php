0f44d93e0496e318414f85cb53b007eca9591110
zverygo <simogin.roman@gmail.com>
2016-08-10 18:52:50 +0300

    start less4

--- www/index.php
+++ www/index.php
@@ -1,20 +1,3 @@
 <?php
-echo "less2.<hr>";
+echo "less4.<hr>";
 ?>
-
-
-<style>
-    .butt {
-        color: darkred;
-        background-color: aqua;
-        text-align: center;
-        width: 100px;
-        height: 35px;
-        border: none;
-    }
-
-</style>
-
-<form action="index.php">
-    <button class="butt" type="submit"> SAVE </button>
-</form>
